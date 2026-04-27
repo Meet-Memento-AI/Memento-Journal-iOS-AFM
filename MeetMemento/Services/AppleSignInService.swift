f29131375f356d8777ef5b65deae870a0b715104
Thomas Medak <thomasmedak1@gmail.com>
2026-04-26 20:38:02 -0500

    fix: remove unsupported ASAuthorizationError case for CI SDK

--- MeetMemento/Services/AppleSignInService.swift
+++ MeetMemento/Services/AppleSignInService.swift
@@ -157,8 +157,8 @@ extension AppleSignInService: ASAuthorizationControllerDelegate {
                     continuation?.resume(throwing: AppleSignInError.notInteractive)
                 case .matchedExcludedCredential:
                     continuation?.resume(throwing: AppleSignInError.matchedExcludedCredential)
-                case .credentialImport, .credentialExport,
-                     .preferSignInWithApple, .deviceNotConfiguredForPasskeyCreation:
+                 case .credentialImport, .credentialExport,
+                     .deviceNotConfiguredForPasskeyCreation:
                     // These are passkey-related errors not applicable to Sign in with Apple
                     continuation?.resume(throwing: AppleSignInError.failed)
                 @unknown default:
